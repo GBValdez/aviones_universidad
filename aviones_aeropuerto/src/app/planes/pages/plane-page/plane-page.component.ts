cd6242cb5770ca9f2d7d85012e9815fcea37ae76
GBValdez <gabrielbenjaminvaldezdeleon@gmail.com>
2024-03-22 01:21:09 -0500

    Logre hacer que la imagen se moviera

--- aviones_aeropuerto/src/app/planes/pages/plane-page/plane-page.component.ts
+++ aviones_aeropuerto/src/app/planes/pages/plane-page/plane-page.component.ts
@@ -1,18 +1,35 @@
-import { Component } from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { BtnUploadFileModule } from '@utils/btn-upload-file/btn-upload-file.module';
 
 @Component({
   selector: 'app-plane-page',
   standalone: true,
-  imports: [BtnUploadFileModule],
+  imports: [BtnUploadFileModule, NgClass, NgStyle],
   templateUrl: './plane-page.component.html',
   styleUrl: './plane-page.component.scss',
 })
 export class PlanePageComponent {
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {}
+
   img: any;
+  imgBlob!: Blob;
+  wImg: number = 0;
+  hImg: number = 0;
   uploadFile(event: FileList | undefined): void {
     if (event) {
       this.img = URL.createObjectURL(event[0]);
+      const imgElement = new Image();
+      imgElement.src = this.img;
+      imgElement.onload = () => {
+        console.log('Image loaded', imgElement.width, imgElement.height);
+        const widthEquivalent =
+          (imgElement.width * window.innerHeight) / imgElement.height;
+
+        this.wImg = widthEquivalent;
+        this.hImg = window.innerHeight;
+      };
     }
   }
 }
